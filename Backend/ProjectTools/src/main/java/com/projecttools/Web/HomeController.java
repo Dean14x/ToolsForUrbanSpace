86f9477ef709a52d7c97db27a2d7aed176174008
Mihail <petrushevskimihail@gmail.com>
2023-05-19 13:01:53 +0200

    fixes

--- Backend/ProjectTools/src/main/java/com/projecttools/Web/HomeController.java
+++ Backend/ProjectTools/src/main/java/com/projecttools/Web/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping(path = "/")
-    public String getH(){
-        return "pace";
+    public String getHome(){
+        return "home";
     }
 }
