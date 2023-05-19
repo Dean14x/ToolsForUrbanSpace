273a5b94791ba9518fd478c6ef1df51f93381b06
Ben <ayy@byom.de>
2023-05-19 11:58:22 +0200

    frontend base

---
+++ Frontend/lab-website/src/App.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "React"
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, {this.state.name}!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
