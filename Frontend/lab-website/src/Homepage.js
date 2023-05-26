e65d60d09ebd9696513c050a6e339cf7878b9329
Shaygi <104856049+Shaygi@users.noreply.github.com>
2023-05-26 13:52:38 +0200

    Homepage design

--- Frontend/lab-website/src/Homepage.js
+++ Frontend/lab-website/src/Homepage.js
@@ -1,4 +1,11 @@
 import React from "react";
+import "./homepage.css";
+import Vektor1 from "./images/Vector1.png";
+import Vektor2 from "./images/Vector2.png";
+import Ellipse1 from "./images/Ellipse1.png";
+import logo2 from "./logos/logo2.png";
+import NetworkMap from "./NetworkMap";
+import Login from "./Login";
 
 // Homepage component
 // This is the homepage of the website
@@ -12,16 +19,34 @@ class Homepage extends React.Component {
             session: ""
         };
     }
+    setPage(page) {
+        this.setState({ page: page });
+    }
+
 
     render() {
+        let page;
+        switch (this.state.page) {
+            case "login":
+                page = <Login app={this} />;
+                break;
+        }
         return (
             <div>
-                <h1>Tools For Urban Spaces</h1>
-                <p>
-                    This is a website is for the Tools For Urban Space project.
-                    You can use the tools provided to plan and organize your lab for your commune.
+                <h1 className={"info_titel"}>SmartCommune</h1>
+                <p className={"info"}>
+                    Your Ressourcechecker
+                    Your Networkanalysis
+                    Your Lab Planer
                 </p>
+                <img className={"deko"} src= {Vektor1} alt="vektor"/>
+                <img className={"deko2"} src= {Vektor2} alt="vektor"/>
+                <img className={"deko3"} src= {Ellipse1} alt="vektor"/>
+                <div className={"buttonLogin"}>
+                    <button onClick={() => this.setPage("login")}>Create your Lab now</button>
+                </div>
             </div>
+
 
         );
     }
--- Frontend/lab-website/src/Navbar.js
+++ Frontend/lab-website/src/Navbar.js
@@ -16,7 +16,7 @@ class Navbar extends React.Component {
     render() {
         return (
             <div className="navbar">
-                <img src= {logo2} alt="logo" width={70} height={70} />
+                <img className={"logo"} src= {logo2} alt="logo" width={70} height={70} />
                 <div>
                     <Link className="navbar-item" to="/">Home</Link>
                     <Link className="navbar-item" to="/overview">Overview</Link>
