85ca665496911aebe9cc8596edfb1d090a2fd444
Vladimir2606 <50133456+Vladimir2606@users.noreply.github.com>
2023-06-09 14:02:01 +0200

    infopages

--- Frontend/lab-website/src/ResourceChecker.js
+++ Frontend/lab-website/src/ResourceChecker.js
@@ -13,45 +13,66 @@ class ResourceChecker extends React.Component {
         };
     }
 
+    setPage(page) {
+        this.setState({ page: page });
+    }
+
     render() {
         return (
             <div>
-                <h1>Info Text</h1>
-
-                <div>
-                    <table>
+                <div className={"info_text"}>
+                    <h2>Info Text - Resourcechecker</h2>
+                    <p>
+                        This is a website for the Tools For Urban Space project.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam id nunc vitae magna aliquam aliquet. Donec euismod
+                        This is a website for the Tools For Urban Space project.
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Nullam id nunc vitae magna aliquam aliquet. Donec euismod
+                    </p>
+                </div>
+                <div className="infopage-container" onClick={() => this.setPage()}>
+                    <div className="infopage-manage-left">
+                        <h2>Manage Network</h2>
+                        <p>
+                        A function that displays a list of available devices and their 
+                        key information such as description, cost and available quantity. 
+                        Users can search, filter and select devices to check their availability and cost.
+                        </p>
+                    </div>
+                    <div className="infopage-manage-right">
+                        <h2>Show Network</h2>
+                            <p>
+                            A function that displays a list of available devices and their 
+                        key information such as description, cost and available quantity. 
+                        Users can search, filter and select devices to check their availability and cost.
+                            </p>
+                    </div>
+                    {/* <table>
                         <tr>
-                            <th>available resources</th>
-                            <th>planned resources</th>
+                            <th>Manage Network</th>
+                            <th>Show Network</th>
                         </tr>
 
                         <tr>
                             <td>
                                 A function that displays a list of available devices and their key
                                 information such as description, cost and available quantity. Users
-                                can search, filter and select devices to check their availability
-                                and cost.
-
-                                A function where users can calculate the running costs of equipment
-                                and projects. Here you can insert fields for entering costs and a
-                                button for calculating the total costs.
+                                can search, filter and select devices to check their availability and cost.
                             </td>
 
                             <td>
-                                A function that displays a list of resources to be purchased and
-                                their key information such as description, cost and quantity required.
-                                Users can insert, remove, move resources to the "existing equipment"
-                                list and mark when these devices should be procured.
-
-                                A function that allows users to calculate the planned costs of
-                                equipment and projects. Fields for entering acquisition costs and
-                                running costs are to be shown here. The total cost of the planned
-                                costs should be added together and displayed. In addition, the total
-                                price should be shown how this relates to the real running costs.
+                                A function that displays a list of resources to be purchased and their
+                                key information such as description, cost and quantity required. Users
+                                can insert, remove, move resources to the "existing equipment" list and
+                                mark when these devices should be procured.
                             </td>
                         </tr>
-                    </table>
+                    </table> */}
 
+                </div>
+                <div className="infopage-bg">
+                    <img src="/static/images/Vector5.png" alt="network-vec5"></img>
                 </div>
             </div>
         );
