2c0c8e2a8575cba12fddb5796e56aaea5b699227
Ben <ayy@byom.de>
2023-06-09 13:36:42 +0200

    added example impressum

--- Frontend/lab-website/src/App.js
+++ Frontend/lab-website/src/App.js
@@ -7,6 +7,7 @@ import ResourceChecker from "./ResourceChecker";
 import RatingNetworkAnalysis from "./RatingNetworkAnalysis";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
+import Impressum from "./Impressum";
 import logo2 from './logos/logo2.png';
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
@@ -46,6 +47,7 @@ class App extends React.Component {
             <Route path="/network" element={<NetworkAnalysis app={this} />} />
             <Route path="/resources" element={<ResourceChecker app={this} />} />
             <Route path="/login" element={<Login app={this} />} />
+            <Route path="/impressum" element={<Impressum app={this} />} />
           </Routes>
           <Footer app={this} />
         </BrowserRouter>
