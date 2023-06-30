1196cb2311b8f8486015d932e239ea2a1ac879b1
l30n4rd0b96 <28777189+l30n4rd0b96@users.noreply.github.com>
2023-07-01 01:16:01 +0200

    ImpressumDatenschutzDILABoration

--- Frontend/lab-website/src/App.js
+++ Frontend/lab-website/src/App.js
@@ -5,13 +5,21 @@ import NetworkAnalysis from "./NetworkAnalysis";
 import NetworkMap from "./NetworkMap";
 import NetworkTable from "./NetworkTable";
 
-import { ResourceChecker, OverviewView, InventoryView, PlannedView, CatalogView } from "./ResourceChecker";
+import {
+  ResourceChecker,
+  OverviewView,
+  InventoryView,
+  PlannedView,
+  CatalogView,
+} from "./ResourceChecker";
 import RatingNetworkAnalysis from "./RatingNetworkAnalysis";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import logo2 from "./logos/logo2.png";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Impressum from "./Impressum";
+import Datenschutz from "./Datenschutz";
+import DILABoration from "./DILABoration";
 
 class App extends React.Component {
   constructor(props) {
@@ -43,15 +51,26 @@ class App extends React.Component {
             <Route path="/network" element={<NetworkAnalysis app={this} />} />
             <Route path="/resources" element={<ResourceChecker app={this} />}>
               <Route path="/resources" element={<OverviewView app={this} />} />
-              <Route path="/resources/inventory" element={<InventoryView app={this} />} />
-              <Route path="/resources/planned" element={<PlannedView app={this} />} />
-              <Route path="/resources/catalog" element={<CatalogView app={this} />} />
+              <Route
+                path="/resources/inventory"
+                element={<InventoryView app={this} />}
+              />
+              <Route
+                path="/resources/planned"
+                element={<PlannedView app={this} />}
+              />
+              <Route
+                path="/resources/catalog"
+                element={<CatalogView app={this} />}
+              />
             </Route>
             <Route path="/login" element={<Login app={this} />} />
 
             <Route path="/networkTable" element={<NetworkTable app={this} />} />
             <Route path="/networkMap" element={<NetworkMap app={this} />} />
             <Route path="/impressum" element={<Impressum app={this} />} />
+            <Route path="/datenschutz" element={<Datenschutz app={this} />} />
+            <Route path="/DILAboration" element={<DILABoration app={this} />} />
           </Routes>
           <Footer app={this} />
         </BrowserRouter>
--- Frontend/lab-website/src/Impressum.js
+++ Frontend/lab-website/src/Impressum.js
@@ -1,55 +1,122 @@
 import React from "react";
-import "./Impressum.css";
+import "./footerSites.css";
 
 class Impressum extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-        };
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
 
-    }
+  render() {
+    return (
+      <div>
+        <div className="impressum">
+          <h1>Impressum</h1>
+          <p>Angaben gemäß § 5 TMG</p>
+          <p>
+            M2C Institut für angewandte Medienforschung GmbH <br />
+            Flughafenallee 10
+            <br />
+            28199 Bremen
+            <br />
+            Deutschland
+          </p>
+          <p>
+            <strong>Kontaktaufnahme:</strong> <br />
+            Telefon: 0421 59055402
+            <br />
+            E-Mail:{" "}
+            <a href="mailto:mkoplin@m2c-bremen.de">mkoplin@m2c-bremen.de</a>
+            <br />
+          </p>
+          <p>
+            <strong>Umsatzsteuer-ID:</strong>
+            <br />
+            Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz:
+            DE260151985
+            <br />
+          </p>
 
-    render() {
-        return (
-            <div className='impressum'>
-                <h1>Impressum</h1>
-                <p>Angaben gemäß § 5 TMG</p>
-                <p>Max Muster <br />
-                    Musterweg<br />
-                    12345 Musterstadt <br />
-                </p>
-                <p>
-                    <strong>Vertreten durch:
-                    </strong><br />
-                    Max Muster<br />
-                </p>
-                <p>
-                    <strong>
-                        Kontakt:
-                    </strong> <br />
-                    Telefon: 01234-789456<br />
-                    Fax: 1234-56789<br />
-                    E-Mail: <a href='mailto:max@muster.de'>max@muster.de</a><br />
-                </p>
-                <p><strong>Umsatzsteuer-ID:
-                </strong><br />
-                    Umsatzsteuer-Identifikationsnummer gemäß §27a Umsatzsteuergesetz: Musterustid.<br /><br />
-                    <strong>Wirtschafts-ID: </strong><br />
-                    Musterwirtschaftsid<br />
-                </p>
-                <p>
-                    <strong>Aufsichtsbehörde:</strong><br />
-                    Musteraufsicht Musterstadt<br />
-                </p><br />
-                Impressum vom
-                <a href="https://www.impressum-generator.de" > Impressum Generator</a >
-                der
-                < a href="https://www.kanzlei-hasselbach.de/standorte/frankfurt/" rel="nofollow" >
-                    Kanzlei Hasselbach, Frankfurt</a >
-            </div >
-        );
-    }
+          <strong>Haftungsausschluss – Disclaimer:</strong>
+          <br />
+          <strong>Haftung für Inhalte</strong>
+          <br />
+          <p>
+            Alle Inhalte unseres Internetauftritts wurden mit größter Sorgfalt
+            und nach bestem Gewissen erstellt. Für die Richtigkeit,
+            Vollständigkeit und Aktualität der Inhalte können wir jedoch keine
+            Gewähr übernehmen. Als Diensteanbieter sind wir gemäß § 7 Abs.1 TMG
+            für eigene Inhalte auf diesen Seiten nach den allgemeinen Gesetzen
+            verantwortlich. Nach §§ 8 bis 10 TMG sind wir als Diensteanbieter
+            jedoch nicht verpflichtet, übermittelte oder gespeicherte fremde
+            Informationen zu überwachen oder nach Umständen zu forschen, die auf
+            eine rechtswidrige Tätigkeit hinweisen. Verpflichtungen zur
+            Entfernung oder Sperrung der Nutzung von Informationen nach den
+            allgemeinen Gesetzen bleiben hiervon unberührt. Eine diesbezügliche
+            Haftung ist jedoch erst ab dem Zeitpunkt der Kenntniserlangung einer
+            konkreten Rechtsverletzung möglich. Bei Bekanntwerden von den o.g.
+            Rechtsverletzungen werden wir diese Inhalte unverzüglich entfernen.
+          </p>
+          <strong>Haftungsbeschränkung für externe Links</strong>
+          <br />
+          <p>
+            Unsere Webseite enthält Links auf externe Webseiten Dritter. Auf die
+            Inhalte dieser direkt oder indirekt verlinkten Webseiten haben wir
+            keinen Einfluss. Daher können wir für die „externen Links“ auch
+            keine Gewähr auf Richtigkeit der Inhalte übernehmen. Für die Inhalte
+            der externen Links sind die jeweilige Anbieter oder Betreiber
+            (Urheber) der Seiten verantwortlich. Die externen Links wurden zum
+            Zeitpunkt der Linksetzung auf eventuelle Rechtsverstöße überprüft
+            und waren im Zeitpunkt der Linksetzung frei von rechtswidrigen
+            Inhalten. Eine ständige inhaltliche Überprüfung der externen Links
+            ist ohne konkrete Anhaltspunkte einer Rechtsverletzung nicht
+            möglich. Bei direkten oder indirekten Verlinkungen auf die Webseiten
+            Dritter, die außerhalb unseres Verantwortungsbereichs liegen, würde
+            eine Haftungsverpflichtung ausschließlich in dem Fall nur bestehen,
+            wenn wir von den Inhalten Kenntnis erlangen und es uns technisch
+            möglich und zumutbar wäre, die Nutzung im Falle rechtswidriger
+            Inhalte zu verhindern. Diese Haftungsausschlusserklärung gilt auch
+            innerhalb des eigenen Internetauftrittes „Name Ihrer Domain“
+            gesetzten Links und Verweise von Fragestellern, Blogeinträgern,
+            Gästen des Diskussionsforums. Für illegale, fehlerhafte oder
+            unvollständige Inhalte und insbesondere für Schäden, die aus der
+            Nutzung oder Nichtnutzung solcherart dargestellten Informationen
+            entstehen, haftet allein der Diensteanbieter der Seite, auf welche
+            verwiesen wurde, nicht derjenige, der über Links auf die jeweilige
+            Veröffentlichung lediglich verweist. Werden uns Rechtsverletzungen
+            bekannt, werden die externen Links durch uns unverzüglich entfernt.
+          </p>
+          <strong>Urheberrecht</strong>
+          <br />
+          <p>
+            Die auf unserer Webseite veröffentlichen Inhalte und Werke
+            unterliegen dem deutschen Urheberrecht (
+            <a href="http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf">
+              http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf
+            </a>
+            ) . Die Vervielfältigung, Bearbeitung, Verbreitung und jede Art der
+            Verwertung des geistigen Eigentums in ideeller und materieller Sicht
+            des Urhebers außerhalb der Grenzen des Urheberrechtes bedürfen der
+            vorherigen schriftlichen Zustimmung des jeweiligen Urhebers i.S.d.
+            Urhebergesetzes (
+            <a href="http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf ">
+              http://www.gesetze-im-internet.de/bundesrecht/urhg/gesamt.pdf 
+            </a>
+            ). Downloads und Kopien dieser Seite sind nur für den privaten und
+            nicht kommerziellen Gebrauch erlaubt. Sind die Inhalte auf unserer
+            Webseite nicht von uns erstellt wurden, sind die Urheberrechte
+            Dritter zu beachten. Die Inhalte Dritter werden als solche kenntlich
+            gemacht. Sollten Sie trotzdem auf eine Urheberrechtsverletzung
+            aufmerksam werden, bitten wir um einen entsprechenden Hinweis. Bei
+            Bekanntwerden von Rechtsverletzungen werden wir derartige Inhalte
+            unverzüglich entfernen. <br />
+            Dieses Impressum wurde freundlicherweise von jurarat.de zur
+            Verfügung gestellt.
+          </p>
+        </div>
+      </div>
+    );
+  }
 }
-
 
 export default Impressum;
