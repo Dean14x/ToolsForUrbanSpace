bd5e69f8ed1c710c729d4e520357124f74e309db
Vladimir2606 <50133456+Vladimir2606@users.noreply.github.com>
2023-06-30 14:06:00 +0200

    m2c und bmbf logos hinzugefügt

--- Frontend/lab-website/src/Footer.js
+++ Frontend/lab-website/src/Footer.js
@@ -15,6 +15,11 @@ class Footer extends React.Component {
       <div className="footer">
         <div className="footer-item">
           <Link className="footer-link" to="/impressum">Impressum</Link>
+          <div id="logos">
+            <a id="links" className="logoTxt">gefördert durch</a>
+            <a className="m2c" id="links" href="https://m2c-bremen.de" target="_blank"><img src="./static/images/logo_m2c.jpg"></img></a>
+            <a className="bmbf" id="links" href="https://www.bmbf.de/bmbf/de/home/home_node.html" target="_blank"><img src="./static/images/347px-BMBF_Logo.svg_.png"></img></a>
+          </div>
         </div>
       </div>
     );
