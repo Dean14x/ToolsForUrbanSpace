0bae4dae079632868f70f82dd898c71d91e84545
4-en <ben.schumacher0@gmail.com>
2023-07-14 21:08:46 +0200

    api test

---
+++ tools/APITest/test_endpoints.py
@@ -0,0 +1,17 @@
+# quick script to test the endpoints of the API
+
+import requests
+import json
+
+def main():
+    """
+    Main function
+    """
+
+    ADDRESS = "http://
+    
+
+
+
+if __name__ == "__main__":
+    main()
